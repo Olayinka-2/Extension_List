a268e5a991c58e73bbba4ebbda0feab58c1b795b
olayinka-2 <olayinkaisrael802@gmail.com>
2025-07-09 14:21:36 +0100

    Add color switch mode

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,19 +1,36 @@
-import sun from "../assets/images/icon-sun.svg";
+import { useEffect, useState } from "react";
+import sunIcon from "../assets/images/icon-sun.svg";
+import moonIcon from "../assets/images/icon-moon.svg";
 import logo from "../assets/images/logo.svg";
-import syles from './Header.module.css';
+import styles from './Header.module.css';
 
 // import { tabIndex } from "./componentUtils";
 
 
 const Header:React.FC = () => {
+  const [isLight, setIsLight] = useState(false);
+
+
+  useEffect(() => {
+    const body = document.body;
+    if (isLight) {
+      body.classList.add("light-mode");
+    } else {
+      body.classList.remove("light-mode");
+    }
+  }, [isLight]);
+
+
   return (
     <header>
-      <div className={syles["header-container"]}>
+      <div className={styles["header-container"]}>
         <div>
           <img src={logo} alt="logo" />
         </div>
-        <div className={syles["toggle-background"]} tabIndex={0}>
-          <img src={sun} alt="LightMode toggle botton" />
+        <div className={`${styles["toggle-background"]} ${styles.themeToggle}`} tabIndex={0}
+        onClick={() => setIsLight(!isLight)}
+        >
+          <img src={isLight ? moonIcon : sunIcon} alt="LightMode toggle botton" />
         </div>
       </div>
     </header>
